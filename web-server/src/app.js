6b431dc7534349a214693a39c47cba46b3a9c815
Simone Ramundi <simone.ramundi@4science.com>
2023-12-18 17:31:41 +0100

    Advanced Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define path for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -18,7 +21,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res)=>{
     res.render('index', {
         title: 'Weather App',
-        name: 'Simone'
+        name: 'Simone R'
     })
 })
 
@@ -31,7 +34,9 @@ app.get('/about', (req, res)=>{
 
 app.get('/help', (req, res)=>{
     res.render('help', {
-        helpText: 'Help... (.__.)"'
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Simone R'
     })
 })
 
