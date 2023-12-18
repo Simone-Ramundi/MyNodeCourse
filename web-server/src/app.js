40c81f652264f130a85d471c976d5c7fbb7c4fdd
Simone Ramundi <simone.ramundi@4science.com>
2023-12-18 16:52:58 +0100

    Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res)=>{
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Simone'
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about', {
+        title: 'About me',
+        name: 'Simone R'
+    })
+})
+
+app.get('/help', (req, res)=>{
+    res.render('help', {
+        helpText: 'Help... (.__.)"'
+    })
+})
 
 app.get('/weather', (req,res) =>{
     res.send({
@@ -12,7 +33,6 @@ app.get('/weather', (req,res) =>{
         location: 'London'
     })
 })
-
 
 
 
