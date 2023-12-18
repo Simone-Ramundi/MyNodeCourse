c0be19d64a053a44ee512bcbb51f0cf04b2e352b
Simone Ramundi <simone.ramundi@4science.com>
2023-12-18 15:08:30 +0100

    Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,18 +3,21 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res)=>{
-    res.send('Hello Express')
+    res.send('<h1>Hello</h1>')
 })
 app.get('/help', (req,res) =>{
-    res.send('Help page')
+    res.send('Help')
 })
 
 app.get('/about', (req,res) =>{
-    res.send('About')
+    res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req,res) =>{
-    res.send('Your weather')
+    res.send({
+        forecast: 23,
+        location: 'London'
+    })
 })
 
 
