2e7c71316b1e48ed419300ac03ecb68595acb96f
Simone Ramundi <simone.ramundi@4science.com>
2024-01-03 16:27:06 +0100

    Rest APIs and Promises

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
-import validator from "validator";
+const mongoose = require('mongoose')
+const validator = require('validator')
 
 const User = mongoose.model('User', {
     name: {
